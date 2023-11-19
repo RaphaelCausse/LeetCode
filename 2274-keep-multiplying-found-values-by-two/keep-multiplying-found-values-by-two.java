d12eb5fda2270536ca895a9a03f78987c6bec417
Raphael CAUSSE <raphael.causse2@gmail.com>
2023-11-19 23:45:58 +0100

    Time: 3 ms (60.55%) | Memory: 44.1 MB (7.26%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -1,5 +1,3 @@
-import java.util.Hashtable;
-
 class Solution
 {
     public int findFinalValue(int[] nums, int original)
@@ -7,13 +5,11 @@ class Solution
         Set<Integer> hashset = new HashSet<>();
         int value = original;
 
-        // Fill hashset with element from input array
         for (int num: nums)
         {
             hashset.add(num);
         }
 
-        // Process of multiplying value by 2 and search it in hashset
         while (hashset.contains(value))
         {
             value = 2 * value;
