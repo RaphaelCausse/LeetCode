962a4d97f6e6ec7f992bad3da70929141acd0b38
Raphael CAUSSE <raphael.causse2@gmail.com>
2023-11-25 00:02:51 +0100

    Time: 16 ms (86.78%) | Memory: 44.8 MB (31.70%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -3,29 +3,34 @@ class Solution
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r)
     {
         List<Boolean> answer = new ArrayList<>();
+        int left;
+        int right;
         int[] subarray;
-        boolean isArithmeticSequence = false;
+        boolean isSequence;
 
         for (int i = 0; i < l.length; i++)
         {
-            // Get the subarray from index l[i] to r[i]
-            subarray = Arrays.copyOfRange(nums, l[i], r[i] + 1);
+            // Get the subarray from index left to right
+            left = l[i];
+            right = r[i];
+            subarray = new int[right - left + 1];
+            for (int j = left; j <= right; j++)
+            {
+                subarray[j - left] = nums[j];
+            }
 
             // Check for an arithmetic sequence
             Arrays.sort(subarray);
+            isSequence = true;
             for (int k = 0; k < subarray.length - 1; k++)
             {
-                if (subarray[k + 1] - subarray[k] == subarray[1] - subarray[0])
+                if (subarray[k + 1] - subarray[k] != subarray[1] - subarray[0])
                 {
-                    isArithmeticSequence = true;
-                }
-                else
-                {
-                    isArithmeticSequence = false;
+                    isSequence = false;
                     break;
                 }
             }
-            answer.add(isArithmeticSequence);
+            answer.add(isSequence);
         }
 
         return answer;
