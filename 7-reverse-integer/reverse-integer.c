4562b38d79e39041c3a4fd3c32baa86a6f2cf8a9
Raphael CAUSSE <raphael.causse2@gmail.com>
2024-10-14 00:01:27 +0200

    Time: 0 ms (100.00%) | Memory: 8.3 MB (93.82%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,19 @@
+int reverse(int x){
+    int reversed = 0;
+
+    while (x != 0)
+    {
+        if (reversed > INT32_MAX / 10 || (reversed == INT32_MAX / 10 && (x % 10) > 7))
+        {
+            return 0;
+        }
+        if (reversed < INT32_MIN / 10 || (reversed == INT32_MIN / 10 && (x % 10) < -8))
+        {
+            return 0;
+        }
+        reversed = (reversed * 10) + (x % 10);
+        x /= 10;
+    }
+
+    return reversed;
+}
